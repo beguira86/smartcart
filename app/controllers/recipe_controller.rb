6c88ee37406ee2eabde8f9cfad08a94190a9bc43
Carl Westmoreland <beguira86@gmail.com>
2015-12-15 15:41:14 -0500

    tweaked auth pages

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,10 +1,9 @@
 class RecipeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:add]
 
   def search
   	instance = FlourPower.new
   	@recipes = instance.search(params[:query])
-
   end
 
   def select
@@ -12,10 +11,9 @@ class RecipeController < ApplicationController
   	@recipe = instance.select(params[:id])
   end
 
-  # def add
-  # 	###LKDNFLKSDNFLKSDNFLKSDF
-  # end
-
+  def add
+  	@recipe = current_user
+  end
 
   private
   def search_params
